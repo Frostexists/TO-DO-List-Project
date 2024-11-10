c4579e0bf7bfcd8591ad788b13101ccf85c257b5
Frost <oyunthewolf@gmail.com>
2024-11-10 18:43:40 +0500

    Added numbering before task, added Edit and Delete Button. And Put the whole code together

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-const addButton = document.getElementById('button');
-const taskList = document.getElementById('task-list');
-const inputField = document.getElementById('input-field');
+const addButton = document.querySelector('.btn.btn--primary');
+const taskList = document.querySelector('.list');
+const inputField = document.querySelector('.input-field');
 
 function addTask() {
     const taskText = inputField.value;
     if (taskText === '') return;
 
     const taskItem = document.createElement('li');
-    taskItem.textContent = taskText;
+    taskItem.classList.add('list__item');
+
+    const taskIndex = document.createElement('span');
+    taskIndex.classList.add('task-index');
+    taskIndex.textContent = taskList.childElementCount + 1;
+
+    const taskDesc = document.createElement('span');
+    taskDesc.classList.add('task-desc');
+    taskDesc.textContent = taskText;
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit-btn');
+    editBtn.innerHTML =  '✏️';
     
     const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = ('Delete');
     deleteBtn.classList.add('delete-btn');
+    deleteBtn.innerHTML = '🗑️';
 
+    taskItem.appendChild(taskIndex);
+    taskItem.appendChild(taskDesc);
+    taskItem.appendChild(editBtn);
     taskItem.appendChild(deleteBtn);
     taskList.appendChild(taskItem);
 
     inputField.value = '';
 
+    editBtn.addEventListener('click', function () {
+        const newText = prompt("Edit your task:", taskDesc.textContent);
+        if (newText) {
+            taskDesc.textContent = newText;
+        }
+    })
+
     deleteBtn.addEventListener('click', function () {
         taskItem.remove();
+        updateTaskIndices();
+    });
+}
+
+function updateTaskIndices() {
+    const tasks = document.querySelectorAll('.task-index');
+    tasks.forEach((task, index) => {
+        tasks.textContent = index + 1;
     });
 }
 
